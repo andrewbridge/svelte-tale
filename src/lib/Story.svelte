61d9e5322a8c09d0d45f894cba4e9ef19930657d
Andrew Bridge <andrew@andrewhbridge.co.uk>
2022-04-05 19:46:02 +0100

    feat: Add structure to Story component and add Storybook like styles

--- src/lib/Story.svelte
+++ src/lib/Story.svelte
@@ -17,10 +17,79 @@
         });
     }
 </script>
-    
-{#if name}<h2>{name}</h2>{/if}
-<svelte:component this={context.component} {...customValues} />
-{#each Object.entries(context.argTypes) as [key, arg]}
-    <label for={key}>{arg.name || key}</label>
-    <input name="{key}" bind:value={customValues[key]} />
-{/each}
+
+<style>
+    h2 {
+        margin: 35px 0 25px;
+    }
+    .canvas {
+        position: relative;
+        overflow: hidden;
+        margin: 25px 0px 40px;
+        border-radius: 4px;
+        background: rgb(255, 255, 255);
+        box-shadow: rgb(0 0 0 / 10%) 0px 1px 3px 0px;
+        border: 1px solid rgba(0, 0, 0, 0.1);
+        padding: 20px;
+    }
+
+    table {
+        border-collapse: collapse;
+        border-spacing: 0px;
+        color: rgb(51, 51, 51);
+        font-size: 13px;
+        line-height: 20px;
+        text-align: left;
+        width: 100%;
+        margin: 25px 1px 40px;
+    }
+
+    tbody {
+        box-shadow: rgb(0 0 0 / 10%) 0px 1px 3px 1px, rgb(0 0 0 / 7%) 0px 0px 0px 1px;
+        border-radius: 4px;
+    }
+
+    tbody tr {
+        background: transparent;
+        overflow: hidden;
+    }
+
+    th, td {
+        padding: 10px 15px;
+    }
+
+    tr:not(:first-of-type) {
+        border-top: 1px solid rgb(230, 230, 230);
+    }
+
+    button {
+        float: right;
+    }
+</style>
+
+<div>
+    {#if name}<h2>{name}</h2>{/if}
+    <div class="canvas">
+        <svelte:component this={context.component} {...customValues} />
+    </div>
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Default</th>
+                <th>Control <button on:click={resetValues}>Reset</button></th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each Object.entries(context.argTypes) as [key, arg]}
+            <tr>
+                <td><label for={key}>{arg.name || key}</label></td>
+                <td>{arg.description || ''}</td>
+                <td>{arg.default || '-'}</td>
+                <td><input name="{key}" bind:value={customValues[key]} /></td>
+            </tr>
+            {/each}
+        </tbody>
+    </table>
+</div>
