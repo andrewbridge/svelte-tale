49f10c4d27324234dfd394e22747a3a4a220d665
Andrew Bridge <andrew@andrewhbridge.co.uk>
2022-04-05 19:25:13 +0100

    feat: Add context types and boilerplate context handlers

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="@sveltejs/kit" />
 
+import type { SvelteComponent } from "svelte";
+
 // See https://kit.svelte.dev/docs/types#the-app-namespace
 // for information about these interfaces
 declare namespace App {
@@ -7,4 +9,30 @@ declare namespace App {
 	// interface Platform {}
 	// interface Session {}
 	// interface Stuff {}
+}
+
+type ArgType = {
+	// The type of input to use for interactive control of this argument
+	control: 'text' | 'number';
+	// The default value for this argument within stories
+	default?: unknown;
+	// A human readable description of the argument
+	description?: string;
+	// A human readable name for the argument
+	name?: string;
+}
+type ArgTypes = { [typeName: string]: ArgType };
+
+type StoryConfig = {
+	// The name used in navigation and headings for story documents
+	title: string;
+	// The group used to nest documents in storybook navigation. Default "Components"
+	group?: string;
+	// The core component used by this story. Allows stories without markup. Optional
+	component?: SvelteComponent;
+	// The arguments used by the core component. Provides the interactive component controls.
+	argTypes?: ArgTypes;
+	// Defines the given story as the default view to be shown in the interactive interface.
+	// The first story found to define this as `true` will be used
+	isStorybookDefault?: boolean;
 }
---
+++ src/lib/stories.ts
@@ -0,0 +1,28 @@
+import type { StoryConfig } from "src/app";
+import { getContext, setContext } from "svelte";
+
+const getDefaultArgs = ({ argTypes }: StoryConfig) => {
+    const defaultArgs = {};
+    if (typeof argTypes !== 'object' || argTypes === null) return defaultArgs;
+    Object.entries(argTypes).forEach(([name, config]) => {
+        if ('default' in config) {
+            defaultArgs[name] = config.default;
+        }
+    })
+    return defaultArgs;
+}
+
+export const toPath = (title) => title.split('/').map(str => (str.toLowerCase().match(/[^\x00-\x2f\x3a-\x40\x5b-\x60\x7b-\x7f]+/g) || []).join('-')).join('/');
+
+const storyContextKey = Symbol();
+export const createStory = (options) => {
+    const context = {
+        title: 'Story',
+        argTypes: {},
+        ...options,
+        defaultArgs: getDefaultArgs(options),
+    };
+    setContext(storyContextKey, context);
+}
+
+export const getStoryContext = () => getContext<StoryConfig>(storyContextKey);
